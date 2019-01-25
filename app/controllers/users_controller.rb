6af77b9ad4848420116fe94d6d34a049157ea352
Emmy White <emmalinerosewhite@gmail.com>
2019-01-25 11:06:42 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,71 @@
 class UsersController < ApplicationController
+  get "/signup" do
+      if logged_in?
+        redirect to '/tweets'
+      else
+       erb :'/users/create_user'
+      end
+    end
 
+     post "/signup" do
+       if params[:username] == "" || params[:password] == "" || params[:email] == ""
+         redirect '/signup'
+      else
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to '/tweets'
+      end
+    end
+
+     get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+  		erb :'/users/show'
+    end
+
+
+     get "/login" do
+        if !logged_in?
+        erb :'/users/login'
+      else
+        redirect '/tweets'
+      end
+    end
+
+     post "/login" do
+      user = User.find_by(:username => params[:username])
+
+       if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/tweets"
+
+       else
+        redirect "/signup"
+      end
+    end
+
+     get "/logout" do
+      if logged_in?
+        session.destroy
+        redirect "/login"
+      else
+        redirect "/"
+      end
+    end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/tweets/tweet'
+  end
 
 end
